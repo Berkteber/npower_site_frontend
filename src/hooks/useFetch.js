17db10ac84d1bbcf97864647c1b0e86981ea40fc
Berkteber <berktbrnecati@gmail.com>
2025-02-10 09:38:19 +0300

    Fetch token update

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,27 +1,35 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-function useFetch(endpoint, baseUrl= 'https://npowersitecms-production.up.railway.app/api') {
+function useFetch(endpoint, baseUrl = "https://npowersitecms-production.up.railway.app/api") {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
-    const api = "https://npowersitecms-production.up.railway.app"
+    const api = "https://npowersitecms-production.up.railway.app";
+
+    // Strapi API Token (Güvenli bir şekilde .env dosyasına koymalısın)
+    const API_TOKEN = process.env.REACT_APP_STRAPI_API_TOKEN;
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`${baseUrl}${endpoint}`);
+                const response = await axios.get(`${baseUrl}${endpoint}`, {
+                    headers: {
+                        Authorization: `Bearer ${API_TOKEN}`,
+                    },
+                });
                 setData(response.data.data);
-            } catch (err){
+            } catch (err) {
                 setError(err);
-            }finally{
+            } finally {
                 setLoading(false);
             }
         };
+
         fetchData();
     }, [endpoint, baseUrl]);
 
-    return{data, loading, error, api}
+    return { data, loading, error, api };
 }
 
 export default useFetch;
